993b1d3fa210f21899f97dfb2a4df51b8c8eb7c0
Kamil Kuzminski <kamil.kuzminski@gmail.com>
2016-01-16 14:41:21 +0100

    Pause other players when the video is playing

--- assets/script.jquery.js
+++ assets/script.jquery.js
@@ -1,16 +1,30 @@
 ;(function ($) {
     $(document).ready(function () {
+        var players = [];
+
+        function pausePlayers() {
+            for (var i = 0; i < players.length; i++) {
+                players[i].api('pause');
+            }
+        }
+
         $('[data-vimeo-video]').each(function () {
             var video = $(this);
             var trigger = video.find('.trigger');
             var iframe = video.find('iframe');
             var player = $f(iframe[0]);
 
+            players.push(player);
+
             if (video.data('vimeo-lightbox')) {
                 player.addEvent('ready', function () {
+                    pausePlayers();
+
+                    // Play the video when opened in lightbox
                     if (iframe.parents('#colorbox').length) {
                         player.api('play');
 
+                        // Autoplay the next video after this one finishes
                         if (video.data('vimeo-lightbox-autoplay')) {
                             player.addEvent('finish', function () {
                                 $.colorbox.next();
@@ -32,6 +46,7 @@
             } else {
                 trigger.on('click', function (e) {
                     e.preventDefault();
+                    pausePlayers();
                     video.addClass('active');
                     player.api('play');
                 });
