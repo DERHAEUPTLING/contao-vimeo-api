1987015d5a099fc6e6b51142eb1439043515ef94
Kamil Kuzminski <kamil.kuzminski@gmail.com>
2016-02-01 17:14:44 +0100

    Fixed the German language file (see #1)

--- languages/de/tl_settings.php
+++ languages/de/tl_settings.php
@@ -16,7 +16,7 @@
  */
 $GLOBALS['TL_LANG']['tl_settings']['vimeo_clientId']     = ['Vimeo client ID', 'Please enter the Vimeo client ID.'];
 $GLOBALS['TL_LANG']['tl_settings']['vimeo_clientSecret'] = ['Vimeo client secret', 'Please enter the Vimeo client secret.'];
-$GLOBALS['TL_LANG']['tl_settings']['vimeo_accessToken']  = ['Vimeo access token', 'Please enter the Vimeo access token (it can be generated in your Vimeo app settings).';
+$GLOBALS['TL_LANG']['tl_settings']['vimeo_accessToken']  = ['Vimeo access token', 'Please enter the Vimeo access token (it can be generated in your Vimeo app settings).'];
 
 
 /**
